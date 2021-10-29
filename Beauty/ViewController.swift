882258bb41e0f9775597b9db4fee46e700263bb5
Sosin Vitalii <vitaliy.sosin@domrf.ru>
2021-10-30 01:34:58 +0300

    Initial Commit

---
+++ Beauty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Beauty
+//
+//  Created by Vitalii Sosin on 30.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
